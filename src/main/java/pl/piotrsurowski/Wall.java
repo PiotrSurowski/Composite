a9cfdd77618c1da0ed01cb8bfd908dc31be21224
PiotrSurowski <piotr.surowski248@gmail.com>
2024-08-11 13:06:54 +0200

    Refaktoryzacja kodu

--- src/main/java/pl/piotrsurowski/Wall.java
+++ src/main/java/pl/piotrsurowski/Wall.java
@@ -8,6 +8,8 @@ import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Wall implements Structure {
 
@@ -18,25 +20,17 @@ public class Wall implements Structure {
     }
     @Override
     public Optional<Block> findBlockByColor(String color) {
-        Optional<Block> result = Optional.empty();
-        for (Block block : this.getAllBlocks(blocks)){
-            if (block.getColor().equalsIgnoreCase(color)){
-                result = Optional.of(block);
-                break;
-            }
-        }
-        return result;
+        return this.getAllBlocks(blocks)
+                .stream()
+                .filter(block -> block.getColor().equalsIgnoreCase(color))
+                .findAny();
     }
 
     @Override
     public List<Block> findBlockByMaterial(String material) {
-        List<Block> result = new LinkedList<>();
-        for (Block block : this.getAllBlocks(blocks)){
-            if (block.getMaterial().equalsIgnoreCase(material)){
-                result.add(block);
-            }
-        }
-        return result;
+        return this.getAllBlocks(blocks)
+                .stream().filter(block -> block.getMaterial().equalsIgnoreCase(material))
+                .collect(Collectors.toList());
     }
 
     @Override
@@ -45,14 +39,9 @@ public class Wall implements Structure {
     }
 
     private List<Block> getAllBlocks(List<Block> blocksIn){
-        List<Block> result = new LinkedList<>();
-        for (Block block : blocksIn){
-            if (block instanceof CompositeBlock){
-                result.addAll(((CompositeBlock) block).getBlocks());
-            } else {
-                result.add(block);
-            }
-        }
-        return result;
+        return blocksIn
+                .stream()
+                .flatMap(block -> block instanceof CompositeBlock ? ((CompositeBlock) block).getBlocks().stream() : Stream.of(block))
+                .collect(Collectors.toList());
     }
 }
